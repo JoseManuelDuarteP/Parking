5487efd6830d2fbd28364823e3ae27438147373a
JoseManuelDuarteP <josduaper@alu.edu.gva.es>
2025-05-26 17:58:23 +0200

    feat: menu completo

---
+++ src/main/java/org/example/MainGrafico.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainGrafico extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("¡Hola, JavaFX funciona!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Mi aplicación JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
